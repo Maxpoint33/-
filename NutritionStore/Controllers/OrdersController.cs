0d2c5aaf1a4cfa3c37f42384f6996ad9f3e8aeb5
Maxpoint33 <maxpoint33676@gmail.com>
2025-04-28 16:14:03 +0300

    AdminDashboard

---
+++ NutritionStore/Controllers/OrdersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NutritionStore.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
